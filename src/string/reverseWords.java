43e6735ec8f3ba5d41d116ed89810af67ddbf9fd
Uday pathak <pathakuday45@gmail.com>
2026-03-24 07:17:30 +0530

    reverse the words in String

--- src/string/reverseWords.java
+++ src/string/reverseWords.java
@@ -2,6 +2,18 @@ package string;
 
 public class reverseWords {
     public static void main(String[] args) {
-
+        String s = "uday   pathak is a good guy";
+        String[] str = s.split(" +");
+        int i = 0, j = str.length - 1;
+        while (i <= j) {
+            String temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+            i++;
+            j--;
+        }
+        String str2 = String.join(" ", str);
+        System.out.println(str2.trim());
     }
+
 }
---
+++ src/string/stringBuilders.java
@@ -0,0 +1,8 @@
+package string;
+
+public class stringBuilders {
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder();
+
+    }
+}
