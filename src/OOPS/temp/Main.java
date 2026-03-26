3cf6a1b7991b15558a91dd4eca28d6e2a9b518ca
Uday pathak <pathakuday45@gmail.com>
2026-03-26 23:45:04 +0530

    addOne

---
+++ src/OOPS/temp/Main.java
@@ -0,0 +1,7 @@
+package OOPS;
+
+public class Main {
+    public static void main(String[] args) {
+        student uday = new student("uday pathak", 22, true, "Mca");
+    }
+}
---
+++ src/string/addOne.java
@@ -0,0 +1,7 @@
+package arrays;
+
+public class addOne {
+    public static void main(String[] args) {
+        
+    }
+}
