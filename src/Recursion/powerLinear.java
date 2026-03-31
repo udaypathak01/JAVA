e2442d15fb09d929cb8b06da6bd8aa5ef96e98e1
Uday pathak <pathakuday45@gmail.com>
2026-03-31 12:31:58 +0530

    powerLinear

---
+++ src/Recursion/powerLinear.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class powerLinear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(pow(a, b));
+    }
+
+    public static int pow(int a, int b) {
+        if (b == 0) return 1;
+        return a * pow(a, b - 1);
+    }
+}
