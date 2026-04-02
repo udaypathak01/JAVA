ccc333754ed4dcca7b60ce8d68325071ed788ad9
Uday pathak <pathakuday45@gmail.com>
2026-04-02 23:17:50 +0530

    NthFibonacciNumber

---
+++ src/Recursion/NthFibpnacciNumber.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class NthFibpnacciNumber {
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+    }
+
+    public static int fib(int n) {
+        if (n == 0 || n == 1) return n;
+        return fib(n - 1) + fib(n - 1);
+    }
+}
