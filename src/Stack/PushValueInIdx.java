429f0b5e89ee96fde89e6e4e701ede35e5819b63
Uday pathak <pathakuday45@gmail.com>
2026-06-27 10:51:39 +0530

    stack

--- src/Stack/PushValueInIdx.java
+++ src/Stack/PushValueInIdx.java
@@ -4,13 +4,28 @@ import java.util.Stack;
 
 public class PushValueInIdx {
     public static void main(String[] args) {
-        Stack<Integer>arr=new Stack<>();
+        Stack<Integer> arr = new Stack<>();
         arr.push(1);
         arr.push(2);
         arr.push(3);
         arr.push(4);
         arr.push(5);
         System.out.println(arr);
+
+        System.out.println(push(arr, 10, 0));
+    }
+
+    public static Stack<Integer> push(Stack<Integer> s, int value, int idx) {
+        Stack<Integer> temp = new Stack<>();
+        int n=s.size();
+        for (int i = 0; i <n-idx ; i++) {
+            temp.push(s.pop());
+        }
+        s.push(value);
+        while(temp.size()!=0){
+            s.push(temp.pop());
+        }
+        return s;
     }
 
 }
